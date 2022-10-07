76a44c3550fbc7268426ae1675342660380572a3
potato-pudding <67677720+potato-pudding@users.noreply.github.com>
2022-10-07 18:20:50 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+direction: "vertical",
+mousewheel: true,
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+},
+});
+
+var swiper = new Swiper('.slider', {
+    loop: true,
+    mousewheel: false,
+    slidesPerView: 1,
+
+    pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: false
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
+
+function move(n) {
+    swiper.slideTo(n, 1000, false)
+}
